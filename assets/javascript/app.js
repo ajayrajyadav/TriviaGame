19cd3aa75e1b5aa944ccdcf50d3dcf03724698d4
Ajay Yadav <ajay.yadav@AWKSOSX014.local>
2019-03-15 14:30:52 -0700

    Initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,127 @@
+$(document).ready(function(){
+    //the game object
+    //https://www.thequiz.com/marvel-movie-quiz-comic-book-fans/
+    var triviaData ={
+        quiz: [{
+            question:"Who kills Agent Phil Coulson in the movie The Avengers?",
+            answerChoices: ["Loki", "Thor", "Nick", "Hulk"],
+            answer: "Loki",
+            imageSource: "q1" 
+        },{
+            question:"Which of the Avengers saves all of Manhattan from the active missile?",
+            answerChoices: ["Thor", "Black Widow", "Iron Man", "Captian America"],
+            answerIndex: "Iron Man",
+            imageSource: "q2" 
+        },{
+            question:"What is the name of Odin’s first child – the Goddess of Death?",
+            answerChoices: ["Helena", "Rona", "Lana", "Hela"],
+            answerIndex: "Hela",
+            imageSource: "q3" 
+        },{
+            question:"Where did Loki send his father while he impersonated him back in Asgard?",
+            answerChoices: ["Alaska", "Norway", "Canada", "Iceland"],
+            answerIndex: "Norway",
+            imageSource: "q4" 
+        },{
+            question:"Which of the Avengers is left half-paralyzed after the airport fight in Captain America: Civil War?",
+            answerChoices: ["Scarlet Witch", "Iron Man", "Rhodey", "Ant-Man"],
+            answerIndex: "Rhodey",
+            imageSource: "q5" 
+        },{
+            question:"What is the name of the agreement the United Nations forced the Avengers to sign?",
+            answerChoices: ["Sokovia Accords", "Sokovia Agreement", "Sokovia Law", "Sokovia Document"],
+            answerIndex: 0,
+            imageSource: "q6" 
+        },{
+            question:"Who was responsible for the experiments that gave Wanda and Pietro their superhuman abilities?",
+            answerChoices: ["Vision", "Strucker", "Steve Rogers", "Nick Fury"],
+            answerIndex: 0,
+            imageSource: "q7" 
+        },{
+            question:"Which of the Infinity Stones is embedded in Vision’s forehead?",
+            answerChoices: ["Life Stone", "Death Stone", "Earth Stone", "Mind Stone"],
+            answerIndex: "Mind Stone",
+            imageSource: "q8" 
+        },{
+            question:"Who is Peter Parker’s best friend in Spider-Man: Homecoming?",
+            answerChoices: ["Ned", "Fred", "Michael", "James"],
+            answerIndex: "Ned",
+            imageSource: "q9" 
+        },{
+            question:"How does Peter end up getting a ride to Washington, D.C to track down Schultz?",
+            answerChoices: ["Borrows tony Stark's Car", "Rejoins his academic decathlon team", "Steals money from aunt May", "Takes a train"],
+            answerIndex: "Rejoins his academic decathlon team",
+            imageSource: "q10" 
+        },{
+            question:"",
+            answerChoices: ["", "", "", ""],
+            answerIndex: 0,
+            imageSource: "" 
+        },{
+            question:"",
+            answerChoices: ["", "", "", ""],
+            answerIndex: 0,
+            imageSource: "" 
+        },{
+            question:"",
+            answerChoices: ["", "", "", ""],
+            answerIndex: 0,
+            imageSource: "" 
+        },{
+            question:"",
+            answerChoices: ["", "", "", ""],
+            answerIndex: 0,
+            imageSource: "" 
+        },{
+            question:"",
+            answerChoices: ["", "", "", ""],
+            answerIndex: 0,
+            imageSource: "" 
+        },{
+            question:"",
+            answerChoices: ["", "", "", ""],
+            answerIndex: 0,
+            imageSource: "" 
+        },{
+            question:"",
+            answerChoices: ["", "", "", ""],
+            answerIndex: 0,
+            imageSource: "" 
+        },{
+            question:"",
+            answerChoices: ["", "", "", ""],
+            answerIndex: 0,
+            imageSource: "" 
+        },{
+            question:"",
+            answerChoices: ["", "", "", ""],
+            answerIndex: 0,
+            imageSource: "" 
+        },{
+            question:"",
+            answerChoices: ["", "", "", ""],
+            answerIndex: 0,
+            imageSource: "" 
+        },{
+            question:"",
+            answerChoices: ["", "", "", ""],
+            answerIndex: 0,
+            imageSource: "" 
+        },{
+            question:"",
+            answerChoices: ["", "", "", ""],
+            answerIndex: 0,
+            imageSource: "" 
+        },{
+            question:"",
+            answerChoices: ["", "", "", ""],
+            answerIndex: 0,
+            imageSource: "" 
+        },{
+            question:"",
+            answerChoices: ["", "", "", ""],
+            answerIndex: 0,
+            imageSource: "" 
+        }],
+    }
+});
